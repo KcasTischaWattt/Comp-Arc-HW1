94705a57b5fac8c1299c0479b46a4f830e66cd81
Matvey Tataurov <71022747+KcasTischaWattt@users.noreply.github.com>
2022-10-23 18:22:13 +0300

    Update input_array_mod.s

--- asm-files/mod/input_array_mod.s
+++ asm-files/mod/input_array_mod.s
@@ -1,76 +1,77 @@
-	.file	"make_array.c"
+	.file	"input_array.c"
 	.intel_syntax noprefix
 	.text
-	.globl	make_array
-	.type	make_array, @function
-make_array:
+	.section	.rodata
+.LC0:
+	.string	"A[%d] ="
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"Incorrect input = %d\n"
+	.text
+	.globl	input_array
+	.type	input_array, @function
+input_array:
 	endbr64
 	push	rbp
 	mov	rbp, rsp
+	sub	rsp, 32
 	mov	QWORD PTR -24[rbp], rdi
 	mov	DWORD PTR -28[rbp], esi
-	mov	QWORD PTR -40[rbp], rdx
-	mov	eax, DWORD PTR -28[rbp]
-	sal	rax, 2
-	lea	rdx, -4[rax]
-	mov	rax, QWORD PTR -24[rbp]
-	add	rax, rdx
-	mov	eax, DWORD PTR [rax]
-	mov	DWORD PTR -4[rbp], eax
-	mov	DWORD PTR -8[rbp], 0
+	mov	DWORD PTR -4[rbp], 0
 	jmp	.L2
-.L5:
-	mov	eax, DWORD PTR -8[rbp]
-	lea	rdx, 0[0+rax*4]
-	mov	rax, QWORD PTR -24[rbp]
-	add	rax, rdx
-	mov	eax, DWORD PTR [rax]
-	test	eax, eax
-	jns	.L3
-	mov	eax, DWORD PTR -8[rbp]
-	lea	rdx, 0[0+rax*4]
-	mov	rax, QWORD PTR -24[rbp]
-	add	rax, rdx
-	mov	eax, DWORD PTR [rax]
-	and	eax, 1
-	test	eax, eax
-	jne	.L3
-	mov	eax, DWORD PTR -8[rbp]
-	lea	rdx, 0[0+rax*4]
-	mov	rax, QWORD PTR -24[rbp]
-	add	rax, rdx
-	mov	eax, DWORD PTR [rax]
-	mov	DWORD PTR -4[rbp], eax
-	jmp	.L4
-.L3:
-	add	DWORD PTR -8[rbp], 1
-.L2:
-	mov	eax, DWORD PTR -8[rbp]
-	cmp	eax, DWORD PTR -28[rbp]
-	jl	.L5
-.L4:
-	mov	DWORD PTR -12[rbp], 0
-	jmp	.L6
-.L7:
-	mov	eax, DWORD PTR -12[rbp]
-	lea	rdx, 0[0+rax*4]
-	mov	rax, QWORD PTR -24[rbp]
-	add	rax, rdx
-	mov	eax, DWORD PTR [rax]
-	mov	edx, DWORD PTR -12[rbp]
-	movsx	rdx, edx
-	lea	rcx, 0[0+rdx*4]
-	mov	rdx, QWORD PTR -40[rbp]
-	add	rdx, rcx
-	imul	eax, DWORD PTR -4[rbp]
-	mov	DWORD PTR [rdx], eax
-	add	DWORD PTR -12[rbp], 1
 .L6:
-	mov	eax, DWORD PTR -12[rbp]
+	mov	eax, DWORD PTR -4[rbp]
+	mov	esi, eax
+	# mov 	esi, DWORD PTR -4[rbp]
+	lea	rdi, .LC0[rip]
+	mov	eax, 0
+	call	printf@PLT
+	mov	eax, DWORD PTR -4[rbp]
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	rsi, rax
+	lea	rdi, .LC1[rip]
+	mov	eax, 0
+	call	__isoc99_scanf@PLT
+	mov	eax, DWORD PTR -4[rbp]
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	eax, DWORD PTR [rax]
+	cmp	eax, 1000
+	jg	.L3
+	mov	eax, DWORD PTR -4[rbp]
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	eax, DWORD PTR [rax]
+	cmp	eax, -1000
+	jge	.L4
+.L3:
+	mov	eax, DWORD PTR -4[rbp]
+	lea	rdx, 0[0+rax*4]
+	mov	rax, QWORD PTR -24[rbp]
+	add	rax, rdx
+	mov	eax, DWORD PTR [rax]
+	mov	esi, eax
+	mov	eax, DWORD PTR [rax]
+	mov	esi, eax
+	# mov esi, DWORD PTR [rax]
+	lea	rdi, .LC2[rip]
+	mov	eax, 0
+	call	printf@PLT
+	mov	eax, 1
+	jmp	.L5
+.L4:
+	add	DWORD PTR -4[rbp], 1
+.L2:
+	mov	eax, DWORD PTR -4[rbp]
 	cmp	eax, DWORD PTR -28[rbp]
-	jl	.L7
-	nop
-	nop
-	pop	rbp
+	jl	.L6
+	mov	eax, 0
+.L5:
+	leave
 	ret
-	.size	make_array, .-make_array
+	.size	input_array, .-input_array
